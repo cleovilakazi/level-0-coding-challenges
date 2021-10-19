0fd376b52c5b81886fb32139c36657a09d1dd155
cleovilakazi <cleopathrastudies@gmail.com>
2021-10-19 20:30:20 +0200

    task0.7

---
+++ level-0-coding-challenges/task0,7.js
@@ -0,0 +1,12 @@
+function celciusToFahrenheit(tempinCel){
+    var farh = (tempinCel * 1.8) + 32 // 1.8 is similar to 9/5 
+    return farh
+}
+
+function farhenheitToCelcius(tempinFarh){
+    var cel = (tempinFarh - 32) * 1.8
+    return cel
+}
+
+celciusToFahrenheit(32);
+farhenheitToCelcius(101);
