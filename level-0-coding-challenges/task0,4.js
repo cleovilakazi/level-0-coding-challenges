3ef01a0e635918f04f46a611d43e9cb1dd78ad23
cleovilakazi <cleopathrastudies@gmail.com>
2021-10-19 20:29:46 +0200

    task0.4

---
+++ level-0-coding-challenges/task0,4.js
@@ -0,0 +1,11 @@
+function evenOrOdd(num){
+    if (num%2 == 1){//using the modulus(%) to get the remainder instead of the division.
+        console.log("odd");
+    }
+    else{
+        console.log("even");
+    }
+
+    
+}
+evenOrOdd(3);
