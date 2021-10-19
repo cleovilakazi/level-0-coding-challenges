0f5f3637c0a2c11ba6d6217aa6c72b93f26ac429
cleovilakazi <cleopathrastudies@gmail.com>
2021-10-19 20:30:11 +0200

    task0.6

---
+++ level-0-coding-challenges/task0,6.js
@@ -0,0 +1,13 @@
+function maximum(num1, num2, num3){
+    if (num1 > num2 && num1 > num3){
+        return num1;
+    }
+    else if (num2 > num1 && num2 > num3){
+        return num2;
+    }
+    else{
+        return num3;
+    }
+
+}
+maximum(17, 12, 4);
