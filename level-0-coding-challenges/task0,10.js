155e15982f3af65c7fe8f7d78c0549e95c53135b
cleovilakazi <91885755+cleovilakazi@users.noreply.github.com>
2021-10-20 10:43:21 +0200

    Update task0,10.js

--- level-0-coding-challenges/task0,10.js
+++ level-0-coding-challenges/task0,10.js
@@ -1,13 +1,13 @@
 function commonCharacters(str1, str2){
     var match = "";
-    var str2 = Array.from(str2); // turn a string into an array of characters.
-    for(x of str1){
+    var str2 = Array.from(str2.toLowerCase()); // turn a string into an array of characters.
+    for(x of str1.toLowerCase()){
         if (str2.includes(x)){
             match = match +  x+",";
             
         }
     }
-    console.log("Common letters: "+ match);
+    console.log("Common letters: "+ match.slice(0 , match.length - 1));
 
 }
-commonCharacters("mbali", "mnisi")
+commonCharacters("Mbali", "mnisi")
