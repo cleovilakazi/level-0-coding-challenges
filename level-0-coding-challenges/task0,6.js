4f8cb0c808d3c3130a359b2e0ee1afd6be1354ae
cleovilakazi <91885755+cleovilakazi@users.noreply.github.com>
2021-10-20 22:01:24 +0200

    Update task0,6.js

--- level-0-coding-challenges/task0,6.js
+++ level-0-coding-challenges/task0,6.js
@@ -1,11 +1,11 @@
 function maximum(num1, num2, num3){
-    if (num1 > num2 && num1 > num3){
+    if (num1 >= num2 && num1 >= num3){
         return num1;
     }
-    else if (num2 > num1 && num2 > num3){
+    else if (num2 >= num1 && num2 >= num3){
         return num2;
     }
-    else{
+    else if (num3 >= num1 && num3 >= num2){
         return num3;
     }
 
