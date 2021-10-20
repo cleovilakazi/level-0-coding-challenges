ac423d93cd97af17f5225227df2a4464d23eef13
cleovilakazi <91885755+cleovilakazi@users.noreply.github.com>
2021-10-20 21:36:36 +0200

    Update task0,9.js

--- level-0-coding-challenges/task0,9.js
+++ level-0-coding-challenges/task0,9.js
@@ -3,11 +3,11 @@ function printVowels(str){
     var match = ""
     for(var x of str){ //for loop to iterate through the values of the string one by one
         if (vowels.includes(x)){
-            match = x.toLowerCase() +","+ match //if the letter is a vowel the it is added into the match var and passed as a string.
+            match = x.toLowerCase() + match //if the letter is a vowel the it is added into the match var and passed as a string.
         }   
     }
     match = new Set(match) // a set does not accept duplicates thefore i created one so that all my vowels can have only one representative.
-    match = [...match].join("")
+    match = [...match].join(",")
     console.log("Vowels:", match))   
 }
 printVowels("Entanglement")
